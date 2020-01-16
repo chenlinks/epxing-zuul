9701157f2232c605a62fd77ca182e6a1feb14871
chenling <chenling@deepexi.com>
2020-01-16 19:53:21 +0800

    code update

--- zuul-code/src/main/java/com/epxing/zuul/config/ZuulWebSecurityConfigurerAdapter.java
+++ zuul-code/src/main/java/com/epxing/zuul/config/ZuulWebSecurityConfigurerAdapter.java
@@ -1,5 +1,6 @@
 package com.epxing.zuul.config;
 
+import com.epxing.zuul.constant.BaseConstants;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,17 +11,23 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Order(1)
 public class ZuulWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/auth-server/auth/login")
+                .antMatchers(BaseConstants.LOGIN_URL)
                 .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .csrf()
-                .disable();
+                .disable()
+                // 退出的URL是/logout
+                .logout().logoutUrl(BaseConstants.LOGOUT_URL).permitAll()
+                // 退出成功后，跳转到/路径。
+                .logoutSuccessUrl(BaseConstants.LOGIN_URL);
     }
 
 }
---
+++ zuul-code/src/main/java/com/epxing/zuul/constant/BaseConstants.java
@@ -0,0 +1,19 @@
+package com.epxing.zuul.constant;
+
+public class BaseConstants  {
+
+        public static final String  LOGIN_URL = "/auth-server/auth/login";
+        public static final String  LOGOUT_URL = "/auth-server/auth/logout";
+
+
+
+
+
+
+
+
+
+
+
+
+}
